07d3d867c135438b8a4d668b52cd2b0133192188
kyutaze <215492008+kyutaze@users.noreply.github.com>
2025-12-19 16:34:59 +0900

    コマンドライン引数の説明を修正: EPUBファイルの説明に「固定レイアウト」を追加

--- epub2img.py
+++ epub2img.py
@@ -29,7 +29,7 @@ def parse_args():
     コマンドライン引数を解析する。
     """
     parser = argparse.ArgumentParser(
-        description="EPUBファイルから画像を抽出し、連番で保存するスクリプト",
+        description="EPUBファイル（固定レイアウト）から画像を抽出し、連番で保存するスクリプト",
         formatter_class=argparse.RawTextHelpFormatter
         )
     parser.add_argument("-i", "--input-epub", required=True, type=Path, help="入力EPUBファイルのパス")
